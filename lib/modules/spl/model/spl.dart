6204e3d26dc902d6378f232c2bb7eb133f2bfb4a
Chabib Nurozak <chabibnurozak@gmail.com>
2024-07-17 21:38:44 +0700

    init

--- lib/modules/spl/model/spl.dart
+++ lib/modules/spl/model/spl.dart
@@ -1,69 +1,94 @@
+import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'spl.g.dart';
 
 @JsonSerializable()
 class SplList {
-    @JsonKey(name: "resp_error")
-    bool? respError;
+  @JsonKey(name: "resp_error")
+  bool? respError;
 
-    @JsonKey(name: "resp_msg")
-    String? respMsg;
+  @JsonKey(name: "resp_msg")
+  String? respMsg;
 
-    @JsonKey(name: "items")
-    List<Spl>? items;
+  @JsonKey(name: "items")
+  List<Spl>? items;
 
-    SplList({
-        this.respError,
-        this.respMsg,
-        this.items,
-    });
+  SplList({
+    this.respError,
+    this.respMsg,
+    this.items,
+  });
 
-    factory SplList.fromJson(Map<String, dynamic> json) => _$SplListFromJson(json);
+  factory SplList.fromJson(Map<String, dynamic> json) => _$SplListFromJson(json);
 
-    Map<String, dynamic> toJson() => _$SplListToJson(this);
+  Map<String, dynamic> toJson() => _$SplListToJson(this);
 }
-
 
 @JsonSerializable()
 class Spl {
-    @JsonKey(name: "spl_id")
-    String? splId;
+  @JsonKey(name: "spl_id")
+  String? splId;
 
-    @JsonKey(name: "spl_nomor")
-    String? splNomor;
+  @JsonKey(name: "spl_nomor")
+  String? splNomor;
 
-    @JsonKey(name: "spl_tanggal")
-    String? splTanggal;
+  @JsonKey(name: "spl_tanggal")
+  String? splTanggal;
 
-    @JsonKey(name: "spl_tgl_spl")
-    String? splTglSpl;
+  @JsonKey(name: "spl_tgl_spl")
+  String? splTglSpl;
 
-    @JsonKey(name: "spl_lama")
-    String? splLama;
+  @JsonKey(name: "spl_lama")
+  String? splLama;
 
-    @JsonKey(name: "spl_keterangan")
-    String? splKeterangan;
+  @JsonKey(name: "spl_keterangan")
+  String? splKeterangan;
 
-    @JsonKey(name: "spl_status")
-    String? splStatus;
+  @JsonKey(name: "spl_status")
+  String? splStatus;
 
-    @JsonKey(name: "spl_alasan_penolakan")
-    String? splAlasanPenolakan;
+  @JsonKey(name: "spl_alasan_penolakan")
+  String? splAlasanPenolakan;
 
+  DateTime? get date {
+    if (splTanggal != null) {
+      return DateTime.parse(splTanggal!);
+    }
+    return null;
+  }
 
-    Spl({
-        this.splId,
-        this.splNomor,
-        this.splTanggal,
-        this.splTglSpl,
-        this.splLama,
-        this.splKeterangan,
-        this.splStatus,
-        this.splAlasanPenolakan,
-    });
+  String get tanggal {
+    if (date != null) {
+      final DateFormat formatter = DateFormat('d MMMM y', 'ID');
+      return formatter.format(date!);
+    }
 
-    factory Spl.fromJson(Map<String, dynamic> json) => _$SplFromJson(json);
+    return '';
+  }
 
-    Map<String, dynamic> toJson() => _$SplToJson(this);
+  String get tanggalSpl {
+    if (splTglSpl != null) {
+      var mDate = DateTime.parse(splTglSpl!);
+      final DateFormat formatter = DateFormat('EEEE, d MMMM y', 'ID');
+      return formatter.format(mDate);
+    }
+
+    return '';
+  }
+
+  Spl({
+    this.splId,
+    this.splNomor,
+    this.splTanggal,
+    this.splTglSpl,
+    this.splLama,
+    this.splKeterangan,
+    this.splStatus,
+    this.splAlasanPenolakan,
+  });
+
+  factory Spl.fromJson(Map<String, dynamic> json) => _$SplFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SplToJson(this);
 }
--- lib/modules/spl/screen/item.dart
+++ lib/modules/spl/screen/item.dart
@@ -8,22 +8,96 @@ class Item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Container(
-          child: Column(
-            children: [
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const SizedBox(
+                  width: 100,
+                  child: Text("Nomor"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text(":"),
+                ),
+                Text("${model.splNomor}"),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 100,
+                  child: Text("Tanggal SPL"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text(":"),
+                ),
+                Text(model.tanggalSpl),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 100,
+                  child: Text("Lama"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text(":"),
+                ),
+                Text("${model.splLama}"),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 100,
+                  child: Text("Keterangan"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text(":"),
+                ),
+                Text("${model.splKeterangan}"),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 100,
+                  child: Text("Status"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text(":"),
+                ),
+                Text("${model.splStatus}"),
+              ],
+            ),
+            if (model.splAlasanPenolakan != null && model.splAlasanPenolakan!.isNotEmpty)
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                width: double.infinity,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    color: Colors.red[200],
+                    border: const Border.fromBorderSide(BorderSide(
+                      color: Colors.red,
+                    ))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Text 1"),
-                    Text("Text 2"),
+                    const Text("Alasan"),
+                    Text("${model.splAlasanPenolakan}"),
                   ],
                 ),
-              ),
-            ],
-          ),
+              )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
